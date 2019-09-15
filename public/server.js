c416e3a86176f851a12fcb858e148b175aa96a4e
unknown <njain2698.nj@gmail.com>
2019-09-15 22:16:10 +0530

    Front

--- public/server.js
+++
@@ -1,9 +0,0 @@
-var rpise={};
-rpise.http=require("http");
-rpise.fs=require("fs");
-rpise.http.createServer(
-    function(request,response){
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.end(rpise.fs.readFileSync("home.html"));
-
-    }).listen(8081,"127.0.0.1");
